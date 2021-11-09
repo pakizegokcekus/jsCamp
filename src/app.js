9ef24a8a2e4b721fc77df07f60dc9ebf3f903eb3
Pakize Kuş <48360370+pakizekus@users.noreply.github.com>
2021-11-09 11:55:36 +0300

    Giriş yapıldı.

---
+++ src/app.js
@@ -0,0 +1,31 @@
+console.log("Merhaba Kodlama.io")
+
+//Js type safe değildir
+let dolarBugun = 9.30 
+let dolarDun =9.20
+
+{
+ let  dolarDun = 9.10
+}
+console.log(dolarDun)
+
+const euroDun=11.2//Sabit
+//euroDun = 11
+
+console.log(euroDun)
+//array 
+//camelCasing
+//PascalCasing
+let konutKredileri=["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredileri"]
+//react
+
+console.log("<ul>")
+
+for (let i = 0; i <konutKredileri.length; i++) {
+
+    console.log("<li>"+konutKredileri[i]+"</li>")
+    
+}
+console.log("</ul>")
+
+console.log(konutKredileri)
